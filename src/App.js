aa9cf053fc8851a399e487127c75cca83b9f5aba
Dhaesha M <dhaesha1@gmail.com>
2022-04-12 23:59:13 -0400

    setting up App js imports

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import React from "react";
+// import GlobalStyle from "./globalStyles";
+// import Homeepagee from "./pages/Home";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return <div className="App">No Error Zone!!</div>;
 }
