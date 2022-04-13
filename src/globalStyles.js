b351b6cd849fc6996e3f3d39d3f5fa03ec7fd155
Dhaesha M <dhaesha1@gmail.com>
2022-04-12 23:30:07 -0400

    preparing js file too handlee global styles using styled components

---
+++ src/globalStyles.js
@@ -0,0 +1,10 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+* {
+    box-sizing: border-border;
+    margin: 0;
+    padding: 0;
+    font-family: 'Montserrat' sans-serif;
+}`;
